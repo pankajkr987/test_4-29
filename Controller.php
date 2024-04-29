c2a4b732c7a931693c5b648849fa81061aa4824b
pankaj_k <fondostech.dev@gmail.com>
2024-04-29 15:03:12 +0530

    by mistake deleted

--- Controller.php
+++ Controller.php
@@ -7,7 +7,3 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-}
