61359c5eabf2a19cc69ed2f8d0be6d6b60dcca8c
pankaj_k <fondostech.dev@gmail.com>
2024-04-29 15:09:36 +0530

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -10,4 +10,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function test(){
+        echo "this is testing function";
+    }
+    
+}
+
+class UserController extends Controller
+{
+    //
+    public function show($id){
+        //return "<h1>called using controller</h1>";
+        return view('about',compact('id'));
+    }
 }
